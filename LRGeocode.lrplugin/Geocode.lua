33122a4f8ef75349ced206389ac6c97d1ae734e6
Dave Townsend <dtownsend@oxymoronical.com>
2012-03-17 19:03:16 -0700

    Support geocoding multiple all selected photos

--- LRGeocode.lrplugin/Geocode.lua
+++ LRGeocode.lrplugin/Geocode.lua
@@ -12,9 +12,19 @@ local LrFunctionContext = import "LrFunctionContext"
 local LrTasks = import("LrTasks")
 local LrHttp = import("LrHttp")
 local LrXml = import("LrXml")
+local LrProgressScope = import("LrProgressScope")
 
 local catalog = LrApplication.activeCatalog()
-local photos = catalog:getTargetPhotos()
+local selected_photos = catalog:getTargetPhotos()
+local photos_to_update = {}
+local all_addresses = {}
+local new_addresses = {}
+local new_address_count = 0
+
+local gps_data = {}
+local location_data = {}
+
+local mainProgress = {}
 
 function url_encode(str)
   str = str:gsub("\n", "\r\n")
@@ -22,6 +32,27 @@ function url_encode(str)
       function (c) return string.format("%%%02X", string.byte(c)) end)
   str = str:gsub(" ", "+")
   return str  
+end
+
+function build_address(photo)
+  local location = location_data[photo].location
+  local city = location_data[photo].city
+  local state = location_data[photo].stateProvince
+  local country = location_data[photo].country
+
+  if country and country ~= "" and state and state ~= "" then
+    local address = state .. ", " .. country
+    if city and city ~= "" then
+      address = city .. ", " .. address
+    end
+    if location and location ~= "" then
+      address = location .. ", " .. address
+    end
+
+    return address
+  end
+
+  return nil
 end
 
 function get_children(element, name)
@@ -85,52 +116,72 @@ function parse_georesult(url)
   end
 end
 
-GeoCode = {}
+function scanForAddresses(context)
+  local progress = LrProgressScope({
+    caption = "Scanning photos for new addresses",
+    functionContext = context,
+    parent = mainProgress,
+    parentEndRange = 0.1,
+  })
 
-function GeoCode.scanImages()
-  if #photos == 1 then
-    local photo = photos[1]
+  for i = 1, #selected_photos do
+    local gps = gps_data[selected_photos[i]].gps
 
-    local gps = photo:getRawMetadata("gps")
-    if gps then
-      return
+    local address = build_address(selected_photos[i])
+
+    if address then
+      if gps == nil then
+        table.insert(photos_to_update, selected_photos[i])
+      end
+
+      if all_addresses[address] == nil then
+        all_addresses[address] = { address = address, gps = gps }
+        if gps == nil then
+          new_addresses[address] = all_addresses[address]
+          new_address_count = new_address_count + 1
+        end
+      elseif gps and all_addresses[address].gps == nil then
+        all_addresses[address].gps = gps
+        new_addresses[address] = nil
+        new_address_count = new_address_count - 1
+      end
     end
 
-    local location = photo:getFormattedMetadata("location")
-    local city = photo:getFormattedMetadata("city")
-    local state = photo:getFormattedMetadata("stateProvince")
-    local country = photo:getFormattedMetadata("country")
+    progress:setPortionComplete(i, #selected_photos)
+    LrTasks.yield()
 
-    if country == nil then
-      return
+    if mainProgress:isCanceled() then
+      return false
     end
+  end
 
-    local address = country
-    if state then
-      address = state .. ", " .. address
-    end
-    if city then
-      address = city .. ", " .. address
-    end
-    if location then
-      address = location .. ", " .. address
-    end
+  return true
+end
 
+function geocodeAddresses(context)
+  local progress = LrProgressScope({
+    caption = "Geocoding addresses",
+    functionContext = context,
+    parent = mainProgress,
+    parentEndRange = 0.9,
+  })
+
+  local pos = 0
+  for address, data in pairs(new_addresses) do
+    pos = pos + 1
     urladdress = url_encode(address)
     local url = "http://maps.googleapis.com/maps/api/geocode/xml?address=" .. urladdress .. "&sensor=false"
     local success, results = LrTasks.pcall(parse_georesult, url)
     if success then
-      if #results == 0 then
-        return
-      end
+      if #results > 0 then
+        local result = LrFunctionContext.callWithContext("selectAddress", function(context, results)
+          if #results == 1 then
+            return results[1]
+          end
 
-      local result = results[1]
-      
-      if #results > 1 then
-        result = LrFunctionContext.callWithContext("selectAddress", function(context, results)
           local f = LrView.osFactory()
           local properties = LrBinding.makePropertyTable(context)
-          properties.result = 1
+          properties.result = 0
 
           local ui = {
             spacing = f:control_spacing(),
@@ -141,6 +192,11 @@ function GeoCode.scanImages()
             f:static_text {
               title = "Select the correct address:"
             },
+            f:radio_button {
+              title = "None of these addresses are correct",
+              value = LrView.bind "result",
+              checked_value = 0,
+            }
           }
 
           for i = 1, #results do
@@ -157,26 +213,108 @@ function GeoCode.scanImages()
           })
 
           if answer ~= "ok" then
+            mainProgress:cancel()
+            return nil
+          end
+
+          if properties.result == 0 then
             return nil
           end
 
           return results[properties.result]
         end, results)
-      end
 
-      if result == nil then
-        return
+        if result then
+          data.address = result.address
+          data.gps = result.gps
+        end
       end
-
-      catalog:withWriteAccessDo("Geocode Photo", function()
-        photo:setRawMetadata("gps", result.gps)
-      end) 
-    else
-      LrDialogs.message(results)
     end
-  else
-    LrDialogs.message("Can only geocode one photo at a time")
+
+    progress:setPortionComplete(pos, new_address_count)
+    LrTasks.yield()
+
+    if mainProgress:isCanceled() then
+      return false
+    end
+  end
+
+  return true
+end
+
+function performUpdates(context)
+  local progress = LrProgressScope({
+    caption = "Applying GPS data",
+    functionContext = context,
+    parent = mainProgress,
+    parentEndRange = 1,
+  })
+
+  local change_count = 0
+
+  for i = 1, #photos_to_update do
+    local address = build_address(photos_to_update[i])
+
+    if all_addresses[address].gps then
+      photos_to_update[i]:setRawMetadata("gps", all_addresses[address].gps)
+      change_count = change_count + 1
+    end
+
+    progress:setPortionComplete(i, #photos_to_update)
+    LrTasks.yield()
+
+    if mainProgress:isCanceled() then
+      error("Operation canceled")
+    end
   end
 end
 
-LrTasks.startAsyncTask(GeoCode.scanImages)
+function updatePhotos(context)
+  catalog:withWriteAccessDo("Geocoding Photos", performUpdates)
+end
+
+LrTasks.startAsyncTask(function()
+  if #selected_photos == 0 then
+    LrDialogs.message("No photos selected")
+  end
+
+  LrFunctionContext.callWithContext("mainTask", function(context)
+    mainProgress = LrProgressScope({
+      title = "Geocoding photos",
+      functionContext = context,
+    })
+    mainProgress:setCancelable(true)
+
+    gps_data = catalog:batchGetRawMetadata(selected_photos, { "gps", })
+
+    if mainProgress:isCanceled() then
+      return
+    end
+    mainProgress:setPortionComplete(2, 100)
+
+    location_data = catalog:batchGetFormattedMetadata(selected_photos, { "location", "city", "stateProvince", "country", })
+
+    if mainProgress:isCanceled() then
+      return
+    end
+    mainProgress:setPortionComplete(5, 100)
+
+    if LrFunctionContext.callWithContext("scanForAddresses", scanForAddresses) == false
+       or mainProgress:isCanceled() or #photos_to_update == 0 then
+      return
+    end
+
+    mainProgress:setPortionComplete(10, 100)
+
+    if new_address_count > 0 then
+      if LrFunctionContext.callWithContext("geocodeAddresses", geocodeAddresses) == false
+         or mainProgress:isCanceled() then
+        return
+      end
+    end
+
+    mainProgress:setPortionComplete(90, 100)
+
+    updatePhotos()
+  end)
+end)
